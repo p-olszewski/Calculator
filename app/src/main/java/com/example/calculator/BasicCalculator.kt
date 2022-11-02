498d1991ad7b239c9ac50d1b0fa91dfdafa0a8e2
p-olszewski <przemo1297@gmail.com>
2022-11-02 20:51:50 +0100

    Refactor numericButtonHandler

--- app/src/main/java/com/example/calculator/BasicCalculator.kt
+++ app/src/main/java/com/example/calculator/BasicCalculator.kt
@@ -12,7 +12,9 @@ private const val TAG = "BasicCalculator"
 class BasicCalculator : AppCompatActivity() {
     lateinit var calc_display: TextView
     var previousValue: String = "0"
-    lateinit var operation : String
+    lateinit var operation: String
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,123 +28,60 @@ class BasicCalculator : AppCompatActivity() {
     }
 
     fun setButtonListeners() {
-        findViewById<Button>(R.id.button_0).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_1).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_2).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_3).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_4).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_5).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_6).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_7).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_8).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_9).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_bksp).setOnClickListener { buttonEventHandler(it) }
-        findViewById<Button>(R.id.button_ac).setOnClickListener { buttonEventHandler(it) }
+        findViewById<Button>(R.id.button_0).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_1).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_2).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_3).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_4).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_5).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_6).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_7).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_8).setOnClickListener { numericButtonHandler(it) }
+        findViewById<Button>(R.id.button_9).setOnClickListener { numericButtonHandler(it) }
     }
 
-    private fun buttonEventHandler(it: View?) {
-        if (it != null) {
+    private fun numericButtonHandler(view: View?) {
+        if (view != null) {
             Log.d(TAG, "Event handler call: ${calc_display.text}")
-            when (it.id) {
-                R.id.button_0 -> {
-                    if (calc_display.text.toString() != "0") {
-                        calc_display.append(getString(R.string.button_0))
-                    }
-                }
-                R.id.button_1 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_1)
-                    } else {
-                        calc_display.append(getString(R.string.button_1))
-                    }
-                }
-                R.id.button_2 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_2)
-                    } else {
-                        calc_display.append(getString(R.string.button_2))
-                    }
-                }
-                R.id.button_3 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_3)
-                    } else {
-                        calc_display.append(getString(R.string.button_3))
-                    }
-                }
-                R.id.button_4 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_4)
-                    } else {
-                        calc_display.append(getString(R.string.button_4))
-                    }
-                }
-                R.id.button_5 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_5)
-                    } else {
-                        calc_display.append(getString(R.string.button_5))
-                    }
-                }
-                R.id.button_6 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_6)
-                    } else {
-                        calc_display.append(getString(R.string.button_6))
-                    }
-                }
-                R.id.button_7 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_7)
-                    } else {
-                        calc_display.append(getString(R.string.button_7))
-                    }
-                }
-                R.id.button_8 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_8)
-                    } else {
-                        calc_display.append(getString(R.string.button_8))
-                    }
-                }
-                R.id.button_9 -> {
-                    if (calc_display.text.toString() == "0") {
-                        calc_display.text = getString(R.string.button_9)
-                    } else {
-                        calc_display.append(getString(R.string.button_9))
-                    }
-                }
-
-                R.id.button_ac -> calc_display.text = "0"
-
-                R.id.button_bksp -> {
-                    val stringLength = calc_display.text.length
-                    if (stringLength <= 1) {
-                        calc_display.text = "0"
-                    } else {
-                        calc_display.text = calc_display.text.substring(0, stringLength - 1)
-                    }
-                }
+            if (calc_display.text.toString() == "0") {
+                calc_display.text = (view as Button).text
+            } else {
+                calc_display.append((view as Button).text)
             }
+            lastNumeric = true;
         }
     }
 
-    fun changeSignBtn(view: View) {}
+    fun changeSignBtn(view: View?) {}
 
-    fun divideBtn(view: View) {}
+    fun divideBtn(view: View?) {}
 
-    fun subtractBtn(view: View) {}
+    fun subtractBtn(view: View?) {}
 
-    fun multiplyBtn(view: View) {}
+    fun multiplyBtn(view: View?) {}
 
-    fun resultBtn(view: View) { }
+    fun resultBtn(view: View?) {}
 
-    fun addBtn(view: View) {
+    fun addBtn(view: View?) {
         previousValue = calc_display.text.toString()
         operation = "+"
     }
 
     fun dotBtn(view: View) {}
 
-
+    fun allClearBtn(view: View?) {
+        Log.d(TAG, "Event handler call: ${calc_display.text}")
+        calc_display.text = "0"
+        lastNumeric = false
+        lastDot = false
+    }
+    fun bkspBtn(view: View?) {
+        Log.d(TAG, "Event handler call: ${calc_display.text}")
+        val stringLength = calc_display.text.length
+        if (stringLength <= 1) {
+            calc_display.text = "0"
+        } else {
+            calc_display.text = calc_display.text.substring(0, stringLength - 1)
+        }
+    }
 }
