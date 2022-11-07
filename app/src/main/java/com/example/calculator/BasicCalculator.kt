b0d9e0bbb30c94fda576e4527ad5401262fa2e87
p-olszewski <przemo1297@gmail.com>
2022-11-07 21:26:29 +0100

    Add methods documentation

--- app/src/main/java/com/example/calculator/BasicCalculator.kt
+++ app/src/main/java/com/example/calculator/BasicCalculator.kt
@@ -17,7 +17,7 @@ class BasicCalculator : AppCompatActivity() {
     private var operation: String = ""
     var lastOperationPressed: Boolean = false
     var dotInValue: Boolean = false
-    var resultValue: Double = 0.0
+    private var resultValue: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +29,15 @@ class BasicCalculator : AppCompatActivity() {
         Log.d(TAG, "First call: ${calcDisplay.text}")
     }
 
-    //TODO add validation to adding to numbers with e notation
-    // f.e. 1.23E13+0,5 = 1.23E13, but result is calculated properly
-    // reading from screen is a problem, should be read from variable
+    /**
+     * Calculates the result and round it to 5 decimal places.
+     * Display result in scientific notation if necessary.
+     * @param value1
+     * @param operator
+     * @param value2
+     */
     private fun calculateResult(value1: Double, operator: String, value2: Double) {
-        Log.d(TAG, "Result value before: ${resultValue}")
+        Log.d(TAG, "Result value before: $resultValue")
         when (operator) {
             "+" -> resultValue = value1 + value2
             "-" -> resultValue = value1 - value2
@@ -43,25 +47,21 @@ class BasicCalculator : AppCompatActivity() {
                     resultValue = value1 / value2
                 } else {
                     Toast.makeText(
-                        applicationContext, "Oops! You better do not divide by 0!", Toast.LENGTH_SHORT
+                        applicationContext, "Oops! You better do not divide by 0! Choose another number.", Toast.LENGTH_SHORT
                     ).show()
                 }
             }
         }
-
-        if (resultValue.rem(1).equals(0.0) or resultValue.rem(1).equals(-0.0)) {
-            calcDisplay.text = resultValue.toLong().toString()
-        } else {
-            calcDisplay.text = resultValue.toString()
-        }
-        if (calcDisplay.text.length > 10) {
-            calcDisplay.text = String.format("%e", resultValue).toDouble().toString()
-        }
-
         dotInValue = calcDisplay.text.toString().contains('.') // true or false
-        Log.d(TAG, "Result: ${resultValue}, $value1 $operator $value2, dotInValue: $dotInValue")
+        formatResult()
+        Log.d(TAG, "Result: $resultValue, $value1 $operator $value2, dotInValue: $dotInValue")
     }
 
+    /**
+     * Allow the user to enter number values
+     * Max number length is set to 10 characters (including a dot).
+     * @param view
+     */
     fun numericButtonHandler(view: View?) {
         if (lastOperationPressed or (calcDisplay.text.toString() == "0")) {
             calcDisplay.text = (view as Button).text
@@ -76,17 +76,18 @@ class BasicCalculator : AppCompatActivity() {
         dotInValue = calcDisplay.text.toString().contains('.') // true or false
         Log.d(
             TAG,
-            "Digit: ${(view as Button).text}, String: ${calcDisplay.text}, lastOperationPressed: ${lastOperationPressed}, dotInValue: $dotInValue"
+            "Digit: ${(view as Button).text}, String: ${calcDisplay.text}, lastOperationPressed: $lastOperationPressed, dotInValue: $dotInValue"
         )
     }
 
+    /**
+     * Allow the user to select a mathematical operation.
+     * calculates the result dynamically with successive operations.
+     * @param view
+     */
     fun operationButtonHandler(view: View?) {
         if (!lastOperationPressed and operation.isNotEmpty()) {
-            if (calcDisplay.text.toString().contains('E')) {
-                calculateResult(firstValue, operation, resultValue)
-            } else {
-                calculateResult(firstValue, operation, calcDisplay.text.toString().toDouble())
-            }
+            calculateResult(firstValue, operation, calcDisplay.text.toString().toDouble())
         }
         if (view != null) {
             when (view.id) {
@@ -100,10 +101,15 @@ class BasicCalculator : AppCompatActivity() {
         firstValue = calcDisplay.text.toString().toDouble()
         Log.d(
             TAG,
-            "Operation: ${operation}, lastOperationPressed: ${lastOperationPressed}, firstValue: ${firstValue}, dotInValue: $dotInValue"
+            "Operation: $operation, lastOperationPressed: $lastOperationPressed, firstValue: $firstValue, dotInValue: $dotInValue"
         )
     }
 
+    /**
+     * Dot button handler.
+     * Allows the user to add dot in expression.
+     * Only 1 dot per expression.
+     */
     fun dotBtn(view: View) {
         if (!dotInValue) {
             calcDisplay.append((view as Button).text)
@@ -111,11 +117,19 @@ class BasicCalculator : AppCompatActivity() {
         }
     }
 
+    /**
+     * All Clear button handler.
+     * Allows the user to clear calculations.
+     */
     fun allClearBtn(view: View?) {
         Log.d(TAG, "Event handler call: ${calcDisplay.text}")
         restoreDefaults()
     }
 
+    /**
+     * Backspace button handler.
+     * Allows the user to delete last character.
+     */
     fun bkspBtn(view: View?) {
         Log.d(TAG, "Event handler call: ${calcDisplay.text}")
         val stringLength = calcDisplay.text.length
@@ -129,6 +143,10 @@ class BasicCalculator : AppCompatActivity() {
         }
     }
 
+    /**
+     * Helper function.
+     * Set essential settings to default.
+     */
     private fun restoreDefaults() {
         calcDisplay.text = "0"
         firstValue = 0.0
@@ -137,25 +155,36 @@ class BasicCalculator : AppCompatActivity() {
         dotInValue = false
     }
 
+    /**
+     * Change sign button handler.
+     * Allows the user to change the sign of a number to the opposite one.
+     */
     fun changeSignBtn(view: View?) {
         var displayedValue = calcDisplay.text.toString().toDouble()
         displayedValue = if (displayedValue > 0) -displayedValue else abs(displayedValue)
+        calcDisplay.text = displayedValue.toString()
+        formatResult()
+    }
 
-        //jeśli reszta dzielenia wartości przez 1 wynosi 0.0 lub -0.0
-        if (displayedValue.rem(1).equals(0.0) or displayedValue.rem(1).equals(-0.0)) {
-            calcDisplay.text = displayedValue.toLong().toString()
-        } else {
-            calcDisplay.text = displayedValue.toString()
+    /**
+     * Helper function.
+     * Format result on screen to scientific notation if needed.
+     * Remove last 2 characters if result ends with '.0'.
+     */
+    private fun formatResult() {
+        calcDisplay.text = String.format("%g", resultValue).toDouble().toString()
+        if (calcDisplay.text.toString().takeLast(2) == ".0") {
+            calcDisplay.text = calcDisplay.text.toString().dropLast(2)
         }
     }
 
+    /**
+     * Result button handler.
+     * Allows the user to calculate result.
+     */
     fun resultBtn(view: View) {
         if (!lastOperationPressed) {
-            if (calcDisplay.text.toString().contains('E')) {
-                calculateResult(firstValue, operation, resultValue)
-            } else {
-                calculateResult(firstValue, operation, calcDisplay.text.toString().toDouble())
-            }
+            calculateResult(firstValue, operation, calcDisplay.text.toString().toDouble())
             lastOperationPressed = true
         }
     }
